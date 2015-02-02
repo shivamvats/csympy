ab272fd37d5b7642ff914c05daea490a06214dbf
Shivam Vats <shivamvats.iitkgp@gmail.com>
2015-02-02 13:34:55 +0530

    Compare hashes

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -18,16 +18,19 @@ std::size_t Symbol::__hash__() const
 bool Symbol::__eq__(const Basic &o) const
 {
     if (is_a<Symbol>(o))
-        return name_ == static_cast<const Symbol &>(o).name_;
+        //return name_ == static_cast<const Symbol &>(o).name_;
+        return (this->__hash__() ==  o.__hash__());
     return false;
 }
 
 int Symbol::compare(const Basic &o) const
 {
     CSYMPY_ASSERT(is_a<Symbol>(o))
-    const Symbol &s = static_cast<const Symbol &>(o);
-    if (name_ == s.name_) return 0;
-    return name_ < s.name_ ? -1 : 1;
+    //const Symbol &s = static_cast<const Symbol &>(o);
+    //if (name_ == s.name_) return 0;
+    std::size_t this_hash = this->__hash__(), o_hash = o.__hash__();
+    if (this_hash == o_hash) return 0;
+    return this_hash < o_hash ? -1 : 1;
 }
 
 std::string Symbol::__str__() const
