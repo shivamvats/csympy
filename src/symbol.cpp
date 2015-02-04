a8dbea03b1abfe8c7575d1253139e3f956e7150c
Shivam Vats <shivamvats.iitkgp@gmail.com>
2015-02-04 17:16:57 +0530

    Replaced __hash__ iwith hash

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -19,7 +19,7 @@ bool Symbol::__eq__(const Basic &o) const
 {
     if (is_a<Symbol>(o))
         //return name_ == static_cast<const Symbol &>(o).name_;
-        return (this->__hash__() ==  o.__hash__());
+        return (this->hash() ==  o.hash());
     return false;
 }
 
@@ -28,9 +28,8 @@ int Symbol::compare(const Basic &o) const
     CSYMPY_ASSERT(is_a<Symbol>(o))
     //const Symbol &s = static_cast<const Symbol &>(o);
     //if (name_ == s.name_) return 0;
-    std::size_t this_hash = this->__hash__(), o_hash = o.__hash__();
-    if (this_hash == o_hash) return 0;
-    return this_hash < o_hash ? -1 : 1;
+    if (this->hash() == o.hash()) return 0;
+    return this->hash() < o.hash() ? -1 : 1;
 }
 
 std::string Symbol::__str__() const
